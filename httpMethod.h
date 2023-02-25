d96cbe236be539708ebd1a507f8e5342b789823b
Jingzhi Zhao <82105180+Jingzhi-cyber@users.noreply.github.com>
2023-02-25 03:32:38 -0500

    Add postRequest

--- httpMethod.h
+++ httpMethod.h
@@ -8,7 +8,7 @@ class HttpMethod {
     public:
     void connectRequest(int server_fd, int client_connection_fd);
     void getRequest(int server_fd, int client_connection_fd,char buffer[], int length);
-    void postRequest(int server_fd, int client_connection_fd);
+    void postRequest(int server_fd, int client_connection_fd, char buffer[], int length);
     void parseHttpResponse(const char* res, size_t size);
 
 };
