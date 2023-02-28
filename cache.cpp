b5bc1fbb9d158e7262f05c1459efd3e2b7dc5e31
Jingzhi <jingzhi.zhao@duke.edu>
2023-02-27 19:55:44 -0500

    Add mutex on cache functions

--- cache.cpp
+++ cache.cpp
@@ -1,23 +1,29 @@
- #include "cache.h"
- #include <iostream> 
- #include <unordered_map> 
+#include "cache.h"
 
-void Cache::put(string request, vector<char> response){
-    responseMap.insert(std::make_pair(request, response));
-    return;
+#include <iostream>
+#include <unordered_map>
+std::mutex cache_mutex;
+void Cache::put(string request, vector<char> response) {
+  std::lock_guard<std::mutex> lock(cache_mutex);
+  responseMap.insert(std::make_pair(request, response));
+  return;
 }
-bool Cache::contains(string request){
-    if (responseMap.count(request) > 0) {
-        return true;
-    } else {
-        return false;
-    }
+bool Cache::contains(string request) {
+  std::lock_guard<std::mutex> lock(cache_mutex);
+  if (responseMap.count(request) > 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-vector<char>* Cache::get(string request) {
-    if (contains(request)) {
-        return &responseMap[request];
-    } else {
-        return nullptr;
-    }
-}  
+vector<char> * Cache::get(string request) {
+  std::lock_guard<std::mutex> lock(cache_mutex);
+  if (contains(request)) {
+    return &responseMap[request];
+  }
+  else {
+    return nullptr;
+  }
+}
