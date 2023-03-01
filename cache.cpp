11e1836ae79c0add1693d106c388c7d28d1692ea
Jingzhi <jingzhi.zhao@duke.edu>
2023-02-28 20:51:06 -0500

    Add renew policy

--- cache.cpp
+++ cache.cpp
@@ -4,15 +4,17 @@
 #include <unordered_map>
 std::mutex cache_mutex;
 void Cache::put(string request, vector<char> response) {
+  if (responseMap.size() >= 100) {
+    responseMap.clear(); //Renew policy
+  }
   std::lock_guard<std::mutex> lock(cache_mutex);
   responseMap.insert(std::make_pair(request, response));
   return;
 }
 
 int Cache::size() {
- return responseMap.size();
+  return responseMap.size();
 }
-
 
 bool Cache::contains(string request) {
   std::lock_guard<std::mutex> lock(cache_mutex);
